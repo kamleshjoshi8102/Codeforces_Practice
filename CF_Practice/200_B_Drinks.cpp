0d786741bef63bbf54332cf4563946e4cbe6d5cc
Aditya Gaikwad <63276445+aditya-gaikwad@users.noreply.github.com>
2021-10-01 14:14:16 +0530

    Create 200_B_Drinks.cpp

---
+++ CF_Practice/200_B_Drinks.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    float sum = 0;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        sum = sum + arr[i];
+    }
+    cout<<sum/n;
+}
