ba1a845eac4dff2d6b3a18df426bb3d4b81fc0b8
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2021-10-11 18:35:45 +0530

    1360_E_Polygon.cpp

---
+++ CF_Practice/1360_E_Polygon.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+#define ll                  long long
+#define en                  '\n'
+#define rep(a,b)            for(ll i=a;i<b;i++)
+#define pb                  push_back
+#define fr                  first
+#define se                  second
+#define all(v)              v.begin(),v.end()
+#define JOSHI               ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+const ll                    mod = 1e9 + 7;
+
+using namespace std;
+
+
+
+
+//              ********************** Functions Start **********************          //
+
+
+
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+
+
+bool isprime(ll n){ 
+    if (n <= 1) return true;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+            for (ll i = 5; i * i <= n; i = i + 6)
+                    { if (n % i == 0 || n % (i + 2) == 0){ return false;}}
+    return true;
+}
+
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+
+bool ispalin(string s)
+{
+    for(ll i=0;i<s.size()/2;i++)
+    {
+        if(s[i]!=s[s.size()-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+//              ********************** Functions End **********************          //
+
+
+
+
+void solve()
+{
+        ll n;
+        cin>>n;
+        char arr[n][n];
+
+        rep(0,n)
+        {
+            string s;
+            cin>>s;
+            for(int j=0;j<s.size();j++)
+            {
+                arr[i][j]=s[j];
+            }
+        }
+
+        // rep(0,n)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         cout<<arr[i][j]<<' ';
+        //     }
+        //     cout<<en;
+        // }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i][j]=='1')
+                {
+                    if(j==n-1 || i==n-1)
+                    {
+                        continue;
+                    }
+                    else if(arr[i][j]=='1')
+                    {
+                        if(arr[i][j+1]=='0' && arr[i+1][j]=='0')
+                        {
+                            cout<<"NO"<<en;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        cout<<"YES"<<en;
+/*
+
+00 01 02 03 04
+10 11 12 13 14
+20 21 22 23 24
+30 31 32 33 34
+40 41 42 43 44
+    
+*/
+
+}
+
+/*   
+     => int overflow
+     => array bounds 
+     => special cases (n=1?)
+     => do smth instead of nothing 
+     => stay organized 
+     => WRITE STUFF DOWN DON'T GET STUCK ON ONE APPROACH
+     =>Read Stuff At Bottom !
+
+
+     * +-----+-----------------+
+     * |  n  | Worst Algorithm |
+     * +-----+-----------------+
+     * | 11  | O(n!), O(n^6)   |
+     * | 18  | O(2^n * n^2)    |
+     * | 22  | O(2^n * n)      |
+     * | 100 | O(n^4)          |
+     * | 400 | O(n^3)          |
+     * | 2K  | O(n^2 log n)    |
+     * | 10K | O(n^2)          |
+     * | 1M  | O(n log n)      |
+     * +-----+-----------------+
+     
+*/
+
+int main(void)
+{
+
+        JOSHI
+
+      // #ifndef ONLINE_JUDGE
+      //  freopen("input.txt", "r", stdin);//for input from input.txt
+      //  freopen("output.txt", "w", stdout); //for Output.txt
+      // #endif
+       
+
+
+      ll test_case=1;
+      cin>>test_case;
+     
+      while(test_case--)
+      {
+
+        solve();
+       
+      }
+      
+
+
+  cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+  return 0;
+
+}
